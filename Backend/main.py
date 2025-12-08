b42dcd974a635e44dd9073b44f4bb771ba04e41d
Krish-Parothi <krrishparothi10@gmail.com>
2025-12-09 04:33:10 +0530

    User Specific Whatsapp Tntegrated

--- Backend/main.py
+++ Backend/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.middleware.error_handler import register_exception_handlers
 from app.routers import auth, expenses, analytics, llm
 from Autocategorization import auth_ai
-
+from Whatsapp_Integration.whatsapp_webhook import router as text_expense_router
 
 app = FastAPI()
 
@@ -23,3 +23,4 @@ app.include_router(expenses.router, prefix="/expenses", tags=["expenses"])
 app.include_router(analytics.router, prefix="/analytics", tags=["analytics"])
 app.include_router(llm.router, prefix="/llm", tags=["llm"])
 app.include_router(auth_ai.router, prefix="/api", tags=["auth-ai"])
+app.include_router(text_expense_router, prefix="/api/whatsapp")
